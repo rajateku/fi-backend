17ff412f6ae3487b0d96150b40f1801e9025cb35
rajateku <rajatekuiitb@gmail.com>
2022-04-10 22:47:44 +0100

    added some more graphs

--- watchlist.py
+++ watchlist.py
@@ -22,6 +22,30 @@ def handle_source_doughnut(company):
     print(response)
     return response
 
+def handle_bugs_source_doughnut(company):
+    print("inside handle_watch_list")
+    all_data = read_write_db.get_all_data(TableName="processed_" + company)
+    sources = []
+    for review in all_data:
+        if review["highlightText"] == "":
+            pass
+        else:
+            sources.append(review["source"])
+    sources_stats = dict(Counter(sources))
+    sources = []
+    stats = []
+    print(sources_stats)
+    for source_stat in sources_stats.items():
+        print(source_stat)
+        sources.append(source_stat[0])
+        stats.append(source_stat[1])
+    response = {"labels": sources, "dataset": stats}
+
+    print(response)
+    return response
+
+
+
 
 def handle_issues_line(company):
     response = read_write_db.get_all_data("processed_" + company )
@@ -89,16 +113,51 @@ def handle_issues_line(company):
     return response
 
 
+
+def feedback_trend(company):
+    response = read_write_db.get_all_data("processed_" + company )
+    pdata = []
+    graph_options = []
+    month_based = {}
+    monthly_issues = []
+
+    for review in response:
+        month = review["created_at"][:7]
+        label = review["labels"][0]
+        month_based.setdefault(month, []).append(label)
+        monthly_issues.append(month)
+
+    # print("month_based")
+
+    graph_labels = sorted(list(month_based.keys()))
+    # print(graph_labels)
+    values = (dict(Counter(monthly_issues)))
+    datasets = []
+    for label in graph_labels:
+        datasets.append(values[label])
+    # print(datasets)
+    response = {"labels" : graph_labels, "datasets" : datasets}
+    print(response)
+
+    return response
+
+
 def handle_wathcList(company_name):
     response_source_doughtnut = handle_source_doughnut(company_name)
     response_issues_line = handle_issues_line(company_name)
 
-    return {"source_doughnut": response_source_doughtnut , "issues_topic_wise" : response_issues_line }
+    response_feedback_trend = feedback_trend(company_name)
+    response_bugs_source_doughnut = handle_bugs_source_doughnut(company_name)
+
+    return {"source_doughnut": response_source_doughtnut , "bugs_by_source_doughnut" : response_bugs_source_doughnut,
+            "issues_topic_wise" : response_issues_line, "feedback_trend" : response_feedback_trend }
 
 
 if __name__ == '__main__':
-    response = handle_wathcList("roundpier")
-    print(response)
+    # response = handle_wathcList("roundpier")
+    # print(response)
+    # feedback_trend("roundpier")
+    handle_bugs_source_doughnut("thursday")
     # sort = sorted(response["pdata"], key=lambda k: k.get('month', 0), reverse=False)
     # labels = []
     # mixed_graph_options = response["mixed_graph_options"]
